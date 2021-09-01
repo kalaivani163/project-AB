36348bc2cdd69dae0dc847398cd12ade10566c40
kalaivani163 <kalaigreen163@gmail.com>
2021-09-01 13:02:36 +0530

    my first

---
+++ src/org/sample/A.java
@@ -0,0 +1,37 @@
+package org.sample;
+
+public class A {
+ public static void main(String[] args) {
+	byte age = 12;
+			System.out.println("my age is 25");
+	short atmpin =7856;
+			System.out.println("my atm pin is"+atmpin);
+	int i=12545;
+	      System.out.println(1+i);
+			
+	long phoneno=8973574167l;
+	   System.out.println("the phone no is"+phoneno);
+	   
+	char c= ('A');
+	   System.out.println("the chracter is"+c);
+	
+	String email="Nisha 24@gmail.com";
+	   System.out.println("the email id is"+email);
+	   
+	float empsal= 1254.32f;
+	System.out.println("the flot no is"+empsal);
+	
+	boolean peremployeestatus=true;
+	System.out.println("per employee status"+peremployeestatus);
+	
+	double empSal = 125.263534;
+			System.out.println("the emp sal is"+empSal);
+			
+		
+	
+
+	
+	
+	
+}
+}
